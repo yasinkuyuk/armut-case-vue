4500072f2fc7a892ee9fb5fc4b705e34030e0da4
yasink7 <yasinkuyuk@gmail.com>
2022-06-05 14:32:10 +0300

    git add for previous commit

---
+++ src/components/CustomTextArea.vue
@@ -0,0 +1,30 @@
+<template>
+  <textarea
+    class="
+      border border-[#C5C6CD]
+      rounded
+      placeholder:text-[#9C9DA9]
+      w-full
+      h-36
+      p-2
+      text-[#9C9DA9]
+      font-medium
+      text-sm
+      placeholder:font-normal
+    "
+    style="resize: none"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script>
+export default {
+  name: "CustomTextArea",
+  props: {
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
---
+++ src/components/ImageRadioInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-myGray">
+    <div class="py-3">
+      <img :src="option.valueImageUrl" class="text-center mx-auto" />
+    </div>
+    <div class="py-3 px-2">
+      <input
+        type="radio"
+        class="accent-myGreen mr-1"
+        :checked="checked"
+        @click="test"
+        aria-label="dasdas"
+      />
+      <label class="text-sm">{{ option.value }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageRadioInput",
+  props: {
+    option: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
