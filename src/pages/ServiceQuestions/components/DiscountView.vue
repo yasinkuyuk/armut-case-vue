30aef083d1d0b1cc8afeaa355e0a2728e6557d88
yasink7 <yasinkuyuk@gmail.com>
2022-06-07 12:26:42 +0300

    v-html logic implemented on discount view

--- src/pages/ServiceQuestions/components/DiscountView.vue
+++ src/pages/ServiceQuestions/components/DiscountView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="bg-bgBlack text-white px-4 py-3 text-center text-sm">
-    <div class="font-bold">
-      Bu hizmet Şubat ayında <span class="text-myGreen">%17</span> indirimli!
-    </div>
+    <div class="font-bold" v-html="messageHTML"></div>
   </div>
 </template>
 
@@ -13,6 +11,24 @@ export default {
     message: {
       type: String,
       default: "",
+    },
+  },
+  computed: {
+    messageHTML() {
+      let indexOfPercentage = this.message.indexOf("%");
+      let text = `<p>${this.message.slice(
+        0,
+        indexOfPercentage
+      )}<span class="text-myGreen">%`;
+
+      for (let i = indexOfPercentage + 1; i < this.message.length; i++) {
+        if (this.message[i] == " ") {
+          text += `</span>${this.message.slice(i, this.message.length)}</p>`;
+          break;
+        }
+        text += this.message[i];
+      }
+      return text;
     },
   },
 };
